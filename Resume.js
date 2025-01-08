179780cf677c152d31e90b144fffd025fa82ef66
Bachskie <153666528+Bachskie@users.noreply.github.com>
2025-01-08 09:46:24 +0100

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var scrollDownArrow = document.querySelector('.scrollDownArrow');
 
     window.addEventListener('scroll', function() {
-        // Adjust the value '100' to the desired scroll distance in pixels
+      
         if (window.pageYOffset > 2200) {
             scrollDownArrow.style.opacity = '0';
         } else {
